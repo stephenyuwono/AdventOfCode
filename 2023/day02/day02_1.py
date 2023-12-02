7da2e1f137e5717a5118dbbdd6e7f233095a79aa
Stephen Yuwono <stephenhaniel@gmail.com>
2023-12-02 00:45:41 -0500

    Day 2 done

---
+++ 2023/day02/day02_1.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+file = 'input_02.txt'
+
+id = 0
+
+# dict of max allowed number of colored stones
+std = {'red':12,'green':13,'blue':14}
+
+with open(file) as f:
+    for l,line in enumerate(f):
+
+        # get the list of games after ': '
+        games = line.split(': ')[1]
+
+        # games as list separated by '; '
+        games = [x.split(', ') for x in games.strip('\n').split('; ')]
+
+        # add or no add?
+        add = True
+
+        for game in games:
+            for rgb in game:
+                # separate stone no. and color
+                num = int(rgb.split(' ')[0])
+                col = rgb.split(' ')[1]
+
+                # check against std
+                if num > std[col]:
+                    add = False
+                    break
+                else:
+                    continue
+
+            # break as soon as game is impossible
+            if not add:
+                break
+
+        if add: id += l+1
+
+print(id)
---
+++ 2023/day02/day02_2.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+file = 'input_02.txt'
+
+id = 0
+
+with open(file) as f:
+    for l,line in enumerate(f):
+
+        # dict of current min number of colored stones
+        power = {'red':0,'green':0,'blue':0}
+
+        # get the list of games after ': '
+        games = line.split(': ')[1]
+
+        # games as list separated by '; '
+        games = [x.split(', ') for x in games.strip('\n').split('; ')]
+
+        for game in games:
+            for rgb in game:
+                # separate stone no. and color
+                num = int(rgb.split(' ')[0])
+                col = rgb.split(' ')[1]
+
+                # check against power, update
+                if num > power[col]:
+                    power[col] = num
+                else:
+                    continue
+
+        id += np.prod(list(power.values()))
+
+print(id)
