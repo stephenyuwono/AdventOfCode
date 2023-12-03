3f5c7a18cef1bf5f54a388b8f2de90eabe15e8fe
Stephen Yuwono <stephenhaniel@gmail.com>
2023-12-03 02:14:47 -0500

    Day 3 part 1 done

---
+++ 2023/day03/day03_1.py
@@ -0,0 +1,47 @@
+import numpy as np
+import re
+
+file = 'input_03.txt'
+
+pattern = set('0123456789.')
+
+num = 0
+
+# feed input grid into a list of strings (y lines, x chars)
+test = []
+with open(file) as f:
+    for line in f:
+        test.append(line.strip('\n'))
+    f.close()
+
+for l,line in enumerate(test):
+    # find occurrences of numbers
+    for x in re.finditer(r'[0-9]+',line):
+
+        xmin = max(0,x.start()-1)
+        ymin = max(0,l-1)
+
+        if x.end()==140:
+            xmax = None
+        else:
+            xmax = x.end()+1
+        if l==139:
+            ymax = None
+        else:
+            ymax = l+1
+
+        # create box surrounding number
+        box = line[xmin:xmax]
+        if l!=0:
+            box = test[ymin][xmin:xmax] + box
+        if l!=139:
+            box +=test[ymax][xmin:xmax]
+
+        # remove number from box, can be skipped
+        # re.sub(r'[0-9]+','',box)
+
+        # if there is a character in the box that is not in the pattern, add
+        if len(set(box)&pattern) < len(set(box)):
+            num += int(x.group())
+
+print(num)
