c88ee948d465a6935deaf38b6a97d3db9d4bc38e
Stephen Yuwono <stephenhaniel@gmail.com>
2024-12-02 20:02:40 -0500

    Day 2 part 2 done

---
+++ 2024/day02/day02_2.py
@@ -0,0 +1,35 @@
+from time import perf_counter
+import numpy as np
+
+start = perf_counter()
+
+file = 'input_02.txt'
+
+with open(file, 'r') as f:
+    safe_count = 0
+    for l,line in enumerate(f):
+        x = np.loadtxt(line.split(),dtype='i8')
+
+        diff = x[1:] - x[:-1]
+
+        if (np.all(diff >= 1) and np.all(diff <= 3)) or (np.all(diff >= -3) and np.all(diff <= -1)):
+            safe_count += 1
+        else:
+            # brute force popping one element at a time
+            x = x.tolist()
+            for i in range(len(x)):
+                tmp = x.copy()
+                tmp.pop(i)
+                tmp = np.array(tmp,dtype='i8')
+                diff = tmp[1:] - tmp[:-1]
+                if (np.all(diff >= 1) and np.all(diff <= 3)) or (np.all(diff >= -3) and np.all(diff <= -1)):
+                    safe_count += 1
+                    break
+
+    print(safe_count)
+
+    f.close()
+
+
+
+print(f'time: {perf_counter()-start:.3f} s')
